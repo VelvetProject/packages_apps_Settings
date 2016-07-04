e1ffe0e109082455dc5812a4f8320e43d9f52433
George G <kreach3r@users.noreply.github.com>
2016-07-04 12:07:11 +0300

    Apps: show/hide Substratum overlays [2/2]
    Default behavior is "hidden" substratum overlays,
    as they pollute the screen, and you can't notice the
    actual apps.
    So we follow the current trend set by Android with
    "show system" and make it "show overlays" on default.
    
    Change-Id: I1cd4d4d3acd767a87fe402c325a872a4c04e0438

--- src/com/android/settings/applications/ManageApplications.java
+++ src/com/android/settings/applications/ManageApplications.java
@@ -116,6 +116,7 @@ public class ManageApplications extends InstrumentedPreferenceFragment
 
     private static final String EXTRA_SORT_ORDER = "sortOrder";
     private static final String EXTRA_SHOW_SYSTEM = "showSystem";
+    private static final String EXTRA_SHOW_SUBSTRATUM = "showSubstratum";
     private static final String EXTRA_HAS_ENTRIES = "hasEntries";
     private static final String EXTRA_HAS_BRIDGE = "hasBridge";
 
@@ -145,7 +146,8 @@ public class ManageApplications extends InstrumentedPreferenceFragment
     public static final int FILTER_APPS_WITH_OVERLAY = 10;
     public static final int FILTER_APPS_WRITE_SETTINGS = 11;
     public static final int FILTER_APPS_INSTALL_SOURCES = 12;
-    public static final int FILTER_APPS_COUNT = 13;  // This should always be the last entry
+    public static final int FILTER_APPS_SUBSTRATUM = 13;
+    public static final int FILTER_APPS_COUNT = 14;  // This should always be the last entry
 
     // Mapping to string labels for the FILTER_APPS_* constants above.
     public static final @IdRes int[] FILTER_LABELS = new int[FILTER_APPS_COUNT];
@@ -220,6 +222,9 @@ public class ManageApplications extends InstrumentedPreferenceFragment
 
     // whether showing system apps.
     private boolean mShowSystem;
+
+    // whether showing substratum overlays.
+    private boolean mShowSubstratum;
 
     private ApplicationsState mApplicationsState;
 
@@ -328,6 +333,7 @@ public class ManageApplications extends InstrumentedPreferenceFragment
         if (savedInstanceState != null) {
             mSortOrder = savedInstanceState.getInt(EXTRA_SORT_ORDER, mSortOrder);
             mShowSystem = savedInstanceState.getBoolean(EXTRA_SHOW_SYSTEM, mShowSystem);
+            mShowSubstratum = savedInstanceState.getBoolean(EXTRA_SHOW_SUBSTRATUM, mShowSubstratum);
         }
 
         mInvalidSizeStr = getActivity().getText(R.string.invalid_size_value);
@@ -522,6 +528,7 @@ public class ManageApplications extends InstrumentedPreferenceFragment
         mResetAppsHelper.onSaveInstanceState(outState);
         outState.putInt(EXTRA_SORT_ORDER, mSortOrder);
         outState.putBoolean(EXTRA_SHOW_SYSTEM, mShowSystem);
+        outState.putBoolean(EXTRA_SHOW_SUBSTRATUM, mShowSubstratum);
         outState.putBoolean(EXTRA_HAS_ENTRIES, mApplications.mHasReceivedLoadEntries);
         outState.putBoolean(EXTRA_HAS_BRIDGE, mApplications.mHasReceivedBridgeCallback);
     }
@@ -649,6 +656,11 @@ public class ManageApplications extends InstrumentedPreferenceFragment
                 && mListType != LIST_TYPE_HIGH_POWER);
         mOptionsMenu.findItem(R.id.hide_system).setVisible(mShowSystem
                 && mListType != LIST_TYPE_HIGH_POWER);
+
+        mOptionsMenu.findItem(R.id.show_substratum).setVisible(!mShowSubstratum
+                && mListType != LIST_TYPE_HIGH_POWER);
+        mOptionsMenu.findItem(R.id.hide_substratum).setVisible(mShowSubstratum
+                && mListType != LIST_TYPE_HIGH_POWER);
     }
 
     @Override
@@ -666,6 +678,11 @@ public class ManageApplications extends InstrumentedPreferenceFragment
             case R.id.show_system:
             case R.id.hide_system:
                 mShowSystem = !mShowSystem;
+                mApplications.rebuild(false);
+                break;
+            case R.id.show_substratum:
+            case R.id.hide_substratum:
+                mShowSubstratum = !mShowSubstratum;
                 mApplications.rebuild(false);
                 break;
             case R.id.reset_app_preferences:
@@ -999,6 +1016,10 @@ public class ManageApplications extends InstrumentedPreferenceFragment
                     filterObj = new CompoundFilter(filterObj,
                             ApplicationsState.FILTER_DOWNLOADED_AND_LAUNCHER);
                 }
+            }
+            if (!mManageApplications.mShowSubstratum) {
+                filterObj = new CompoundFilter(filterObj,
+                        ApplicationsState.FILTER_SUBSTRATUM);
             }
             switch (mLastSortMode) {
                 case R.id.sort_order_size:
